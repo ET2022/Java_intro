c14e51b6719c7a5c7354fb653303c93a9a7494bc
Екатерина <ekattaseva@gmail.com>
2022-02-02 21:28:05 +0200

    Homework1

--- java/src/main/java/automation/qa/MyFirstJava.java
+++ java/src/main/java/automation/qa/MyFirstJava.java
@@ -9,11 +9,11 @@ public class MyFirstJava {
        System.out.println("Please enter your name: ");
 
        Scanner scanner = new Scanner(System.in);
-       String name = scanner.next();
+       String name = scanner.nextLine();
 
        System.out.println("Please enter your favourite hobby: ");
-       String hobby = scanner.next();
+       String hobby = scanner.nextLine();
 
-       System.out.println("Your name is " + name + " and your favourite hobby is " + hobby);
+       System.out.println("Your name is " + name + " and your favourite hobby is " + hobby + "!");
     }
 }
